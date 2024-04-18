946fdea9a87fc63ebb269228f73c641b2f6e95eb
henrykim12 <henrykim966@gmail.com>
2024-04-17 20:09:36 -0700

    change board from matrix<int> to matrix<string>

--- src/solver.cpp
+++ src/solver.cpp
@@ -9,46 +9,53 @@
 using namespace std;
 
 Solver::Solver() {
-    _board = vector<vector<int>>();
+    _board = vector<vector<string>>();
 }
 
-Solver::Solver(vector<vector<int>> start_board) {
+Solver::Solver(vector<vector<string>> start_board) {
     _board = start_board;
 }
 
-bool Solver::isValid(vector<vector<int>> board) {
+bool Solver::isValid(vector<vector<string>> board) {
     
     int numRows = size(board);
     int numCols = size(board[0]);
-    unordered_map<int, set<int>> rows; 
-    unordered_map<int, set<int>> cols;
-    map<tuple<int, int>, set<int>> squares;
+    unordered_map<int, set<string>> rows; 
+    unordered_map<int, set<string>> cols;
+    map<tuple<int, int>, set<string>> squares;
     try {
         for (int r = 0; r < numRows; r++) {
             for (int c = 0; c < numCols; c++) {
-                int value = board[r][c];
+                string value = board[r][c];
+                if (value == "") {
+                    continue;
+                }
                 int rowIndex = r / 3;
                 int colIndex = c / 3;
                 tuple<int, int> pos = make_tuple(rowIndex, colIndex);
 
+                // grab set of numbers in the row r
                 auto rowIter = rows.find(r);
-                set<int> currRow;
+                set<string> currRow;
                 if (rowIter != rows.end()) {
                     currRow = rowIter->second;
                 }
                 
+                // grab set of numbers in the col c
                 auto colIter = cols.find(c);
-                set<int> currCol;
+                set<string> currCol;
                 if (colIter != cols.end()) {
                     currCol = colIter->second;
                 }
 
+                // grab set of numbers in the square pos
                 auto squareIter = squares.find(pos);
-                set<int> currSquare;
+                set<string> currSquare;
                 if (squareIter != squares.end()) {
                     currSquare = squareIter->second;
                 }
 
+                // if curr value is in either of row, col, or square, it is a dupe -> return false
                 if (currRow.find(value) != currRow.end() || currCol.find(value) != currCol.end() || currSquare.find(value) != currSquare.end()) {
                     return false;
                 }
@@ -61,14 +68,14 @@ bool Solver::isValid(vector<vector<int>> board) {
     return true;
 }
 
-vector<vector<int>> Solver::solveBoard(vector<vector<int>> board) {
+vector<vector<string>> Solver::solveBoard(vector<vector<string>> board) {
     return board;
 }
 
-vector<vector<int>> Solver::generateRandomBoard() {
+vector<vector<string>> Solver::generateRandomBoard() {
     return;
 } 
 
-vector<vector<int>> Solver::backtrack() {
+vector<vector<string>> Solver::backtrack() {
     return;
 }
--- src/solver.hpp
+++ src/solver.hpp
@@ -10,7 +10,7 @@ class Solver {
         /*
         The sudoku board
         */
-        vector<vector<int>> _board;
+        vector<vector<string>> _board;
     public:
         /*
         Creates a new solver with a random, generated board
@@ -20,27 +20,27 @@ class Solver {
         /*
         Creates a new solver with a given board
         */
-        Solver(vector<vector<int>> board);
+        Solver(vector<vector<string>> board);
 
         /*
         Return whether the given board is valid or not
         */
-        bool isValid(vector<vector<int>> board);
+        bool isValid(vector<vector<string>> board);
 
         /*
         Return the solved sudoku board, if the board is valid
         */
-        vector<vector<int>> solveBoard(vector<vector<int>> board);
+        vector<vector<string>> solveBoard(vector<vector<string>> board);
 
         /*
         Generate a random, valid sudoku board. Returns 17/81 spots filled
         */
-        vector<vector<int>> generateRandomBoard();
+        vector<vector<string>> generateRandomBoard();
 
         /*
         Backtracking algorithm to fill the board
         */
-        vector<vector<int>> backtrack();
+        vector<vector<string>> backtrack();
 };
 
 #endif
